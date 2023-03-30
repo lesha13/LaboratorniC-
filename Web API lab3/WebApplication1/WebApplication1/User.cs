c2692b5749e671ab68020f401249c28ac73f0626
lesha13 <leshamashuna@gmail.com>
2023-03-30 13:35:30 +0300

    lab3

---
+++ Web API lab3/WebApplication1/WebApplication1/User.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1
+{
+    public class User
+    {
+        public string Username { get; set; } = String.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt  { get; set; }
+
+    }
+}
